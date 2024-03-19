f73ecbc21221f71d9bccf78ef5c1354bd8e74f78
Mikael Bonatti <74106@PROWAY.TREINA>
2024-03-18 22:10:45 -0300

    Atualização da Main

--- src/br/com/projetoMVC/Main.java
+++ src/br/com/projetoMVC/Main.java
@@ -9,6 +9,10 @@ public class Main {
 		
 		ProdutoController controller = new ProdutoController();
 		
+		Produto p = new Produto();
+		p.setDescricao("Mouse");
+		controller.cadastrar(p);
+		
 		System.out.println("- Lista de Produtos -");
 		System.out.println("Id Descrição");
 		for (Produto produto : controller.listarTodos()) {
